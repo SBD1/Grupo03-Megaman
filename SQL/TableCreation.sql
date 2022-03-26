5aae43bb21e6a03eaf926a129e5288e6b8dc1750
Wagner Martins <47457792+wagnermc506@users.noreply.github.com>
2022-03-26 11:26:07 -0300

    Atualização da criação das tabelas
    - Ordem de criação das tabelas modificada
    - Adicionados comandos de transação para o script

--- SQL/TableCreation.sql
+++ SQL/TableCreation.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 CREATE DOMAIN ITEMTYPE 
 	AS VARCHAR(10) NOT NULL
 	CHECK (VALUE IN ('equip', 'consum', 'chave'));
@@ -15,13 +17,6 @@ CREATE DOMAIN PRIM_STAT
 CREATE DOMAIN SEC_STAT
 	AS SMALLINT NOT NULL 
 	CHECK (VALUE >= 0 AND VALUE < 301);
-	
-CREATE TABLE loja (
-	codigo SERIAL,
-	nome VARCHAR(50) NOT NULL,
-	
-	CONSTRAINT loja_pk PRIMARY KEY (codigo)
-);
 
 CREATE TABLE item (
 	id BIGSERIAL,
@@ -30,50 +25,35 @@ CREATE TABLE item (
 	CONSTRAINT item_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE inventario (
+CREATE TABLE consumivel (
+	id SERIAL CONSTRAINT consumivel_pk PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	descricao VARCHAR(200),
+	valor_compra SMALLINT,
+	valor_venda SMALLINT,
+	modificador_dano SMALLINT,
+	modificador_energia SMALLINT,
+	
+	CHECK  ((valor_compra > 0 ) AND (valor_venda > 0))
+		
+);
+
+CREATE TABLE chave (
 	id SERIAL,
-	tamanho SMALLINT,
+	nome VARCHAR(50) NOT NULL,
+	descricao VARCHAR(200),
+	valor_compra SMALLINT,
+	valor_venda SMALLINT,
 	
-	CONSTRAINT inventario_tamanho_ck CHECK (tamanho >= 0 AND tamanho <= 9999),
-	CONSTRAINT inventario_pk PRIMARY KEY (id)
+	CHECK ((valor_compra > 0) AND (valor_venda > 0)),
+	CONSTRAINT chave_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE personagem (
-	codigo SERIAL,
-	tipo CHARTYPE,
+CREATE TABLE equip (
+	id BIGINT,
+	tipo VARCHAR(10) NOT NULL CHECK (tipo IN ('arma', 'armadura')),
 	
-	CONSTRAINT personagem_pk PRIMARY KEY (codigo)
-);
-
-CREATE TABLE player (
-	nome VARCHAR(100),
-	inventario INTEGER,
-	hp PRIM_STAT,
-	energia PRIM_STAT,
-	ataque SEC_STAT,
-	defesa SEC_STAT,
-	evasao SEC_STAT,
-	agilidade SEC_STAT,
-	creditos INTEGER,
---	arma
---	armadura
-	
-	CONSTRAINT player_pk PRIMARY KEY (nome),
-	CONSTRAINT player_credits_ck CHECK(creditos >= 0 AND creditos <= 99999999)
-);
-
-CREATE TABLE mapa (
-	nome VARCHAR(100),
-	descricao VARCHAR(500),
-	
-	CONSTRAINT mapa_pk PRIMARY KEY (nome)
-);
-
-CREATE TABLE habilidade (
-	codigo SERIAL,
-	descricao VARCHAR(400),
-	
-	CONSTRAINT habilidade_pk PRIMARY KEY (codigo)
+	CONSTRAINT equip_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE armadura (
@@ -110,25 +90,70 @@ CREATE TABLE arma (
 		
 );
 
-CREATE TABLE consumivel (
-	id SERIAL CONSTRAINT consumivel_pk PRIMARY KEY,
-	nome VARCHAR(50) NOT NULL,
-	descricao VARCHAR(200),
-	valor_compra SMALLINT,
-	valor_venda SMALLINT,
-	modificador_dano SMALLINT,
-	modificador_energia SMALLINT,
+CREATE TABLE inventario (
+	id SERIAL,
+	tamanho SMALLINT CHECK (tamanho > 0 AND tamanho <= 9999),
 	
-	CHECK  ((valor_compra > 0 ) AND (valor_venda > 0))
+	CONSTRAINT inventario_pk PRIMARY KEY (id)
 );
 
+CREATE TABLE slot (
+	id_inventario INT,
+	pos SMALLINT,
+	item BIGINT,
+	
+	CONSTRAINT slot_pk PRIMARY KEY (id_inventario, pos),
+	CONSTRAINT slot_inventario_fk FOREIGN KEY (id_inventario) 
+		REFERENCES inventario (id),
+	CONSTRAINT slot_item_fk FOREIGN KEY (item)
+		REFERENCES item (id)
+);
 
-CREATE TABLE venda (
+CREATE TABLE personagem (
+--	codigo SERIAL,
+	nome VARCHAR(100),
+	tipo CHARTYPE,
+	
+	CONSTRAINT personagem_pk PRIMARY KEY (nome)
+);
 
-	id_loja INTEGER CONSTRAINT id_loja_fk REFERENCES loja (codigo),
-	id_item INTEGER CONSTRAINT id_item_fk REFERENCES item (id),
+CREATE TABLE player (
+	nome VARCHAR(100),
+	inventario INTEGER,
+	hp PRIM_STAT,
+	energia PRIM_STAT,
+	ataque SEC_STAT,
+	defesa SEC_STAT,
+	evasao SEC_STAT,
+	agilidade SEC_STAT,
+	creditos INTEGER,
+--	arma TODO
+--	armadura
+	
+	CONSTRAINT player_pk PRIMARY KEY (nome),
+	CONSTRAINT player_credits_ck CHECK(creditos >= 0 AND creditos <= 99999999)
+);
 
-	CONSTRAINT venda_pk PRIMARY KEY (id_loja, id_item)
+CREATE TABLE npc (
+	nome VARCHAR(100),
+	inventario INTEGER,
+	hp PRIM_STAT,
+	energia PRIM_STAT,
+	ataque SEC_STAT,
+	defesa SEC_STAT,
+	evasao SEC_STAT,
+	agilidade SEC_STAT,
+--	arma
+--	armadura
+	
+	CONSTRAINT npc_pk PRIMARY KEY (nome)
+);
+
+CREATE TABLE mapa (
+	nome VARCHAR(100),
+	descricao VARCHAR(500),
+	
+	CONSTRAINT mapa_pk PRIMARY KEY (nome)
 );
 
 CREATE TABLE area (
@@ -148,29 +173,62 @@ CREATE TABLE quadrado (
 	area VARCHAR(50),
 	mapa VARCHAR(100),
 	tipo CHAR(1),
-	chance_batalha SMALLINT,
+	chance_batalha SMALLINT CHECK(chance_batalha >= 0 AND chance_batalha <= 100),
 	
 	CONSTRAINT quadrado_pk PRIMARY KEY (pos_x, pos_y, area, mapa),
-	CONSTRAINT quadrado_area_fk FOREIGN KEY (area, mapa)
+	CONSTRAINT quadrado_area FOREIGN KEY (area, mapa)
 		REFERENCES area (nome, mapa)
 );
 
-CREATE TABLE chave (
-	id SERIAL,
-	nome VARCHAR(50) NOT NULL,
-	descricao VARCHAR(200),
-	valor_compra SMALLINT,
-	valor_venda SMALLINT,
+CREATE TABLE conecta (
+	pos_x1 SMALLINT,
+	pos_y1 SMALLINT,
+	area1 VARCHAR(50),
+	mapa1 VARCHAR(100),
+	pos_x2 SMALLINT,
+	pos_y2 SMALLINT,
+	area2 VARCHAR(50),
+	mapa2 VARCHAR(100),
 	
-	CHECK ((valor_compra > 0) AND (valor_venda > 0)),
-	CONSTRAINT chave_pk PRIMARY KEY (id)
+	CONSTRAINT conecta_pk PRIMARY KEY (pos_x1, pos_y1, area1, mapa1, pos_x2, pos_y2, area2, mapa2),
+	CONSTRAINT quadrado1_fk FOREIGN KEY (pos_x1, pos_y1, area1, mapa1)
+		REFERENCES quadrado (pos_x, pos_y, area, mapa),
+	CONSTRAINT quadrado2_fk FOREIGN KEY (pos_x2, pos_y2, area2, mapa2)
+		REFERENCES quadrado (pos_x, pos_y, area, mapa)
 );
 
+CREATE TABLE altera (
+	chave INTEGER,
+	pos_x SMALLINT,
+	pos_y SMALLINT,
+	area VARCHAR(50),
+	mapa VARCHAR(100),
+
+	CONSTRAINT altera_pk PRIMARY KEY (chave, pos_x, pos_y, area, mapa),
+	CONSTRAINT altera_chave_fk FOREIGN KEY (chave)
+		REFERENCES chave (id),
+	CONSTRAINT altera_quadrado_fk FOREIGN KEY (pos_x, pos_y, area, mapa)
+		REFERENCES quadrado (pos_x, pos_y, area, mapa)
+);
+
+CREATE TABLE destranca (
+	chave INTEGER,
+	pos_x SMALLINT,
+	pos_y SMALLINT,
+	area VARCHAR(50),
+	mapa VARCHAR(100),
+
+	CONSTRAINT destranca_pk PRIMARY KEY (chave, pos_x, pos_y, area, mapa),
+	CONSTRAINT destranca_chave_fk FOREIGN KEY (chave)
+		REFERENCES chave (id),
+	CONSTRAINT altera_quadrado_fk FOREIGN KEY (pos_x, pos_y, area, mapa)
+		REFERENCES quadrado (pos_x, pos_y, area, mapa)
+);
 
 CREATE TABLE usa (
 
 	id_chave INTEGER CONSTRAINT id_loja_fk REFERENCES chave (id),
-	id_player VARCHAR(50) CONSTRAINT id_item_fk REFERENCES player (nome),
+	id_player VARCHAR(100) CONSTRAINT id_item_fk REFERENCES player (nome),
 
 	CONSTRAINT usa_pk PRIMARY KEY (id_chave, id_player)
 	
@@ -178,31 +236,41 @@ CREATE TABLE usa (
 	-- da porta e salvar no histórico destranca
 );
 
-CREATE TABLE npc (
-	nome VARCHAR(100),
-	inventario INTEGER,
-	hp PRIM_STAT,
-	energia PRIM_STAT,
-	ataque SEC_STAT,
-	defesa SEC_STAT,
-	evasao SEC_STAT,
-	agilidade SEC_STAT,
---	arma
---	armadura
+CREATE TABLE habilidade (
+	codigo SERIAL,
+	descricao VARCHAR(400),
 	
-	CONSTRAINT npc_pk PRIMARY KEY (nome)
+	CONSTRAINT habilidade_pk PRIMARY KEY (codigo)
 );
 
-CREATE TABLE slot (
-	id_inventario INT,
-	pos SMALLINT,
-	item BIGINT,
+CREATE TABLE loja (
+	codigo SERIAL,
+	nome VARCHAR(50) NOT NULL,
 	
-	CONSTRAINT slot_pk PRIMARY KEY (id_inventario, pos),
-	CONSTRAINT slot_inventario_fk FOREIGN KEY (id_inventario) 
-		REFERENCES inventario (id),
-	CONSTRAINT slot_item_fk FOREIGN KEY (item)
+	CONSTRAINT loja_pk PRIMARY KEY (codigo)
+);
+
+CREATE TABLE estoque (
+-- Nos outros documentos, este é o relacionamento "venda"
+	id_loja INTEGER CONSTRAINT id_loja_fk REFERENCES loja (codigo),
+	id_item INTEGER CONSTRAINT id_item_fk REFERENCES item (id),
+
+	CONSTRAINT estoque_pk PRIMARY KEY (id_loja, id_item),
+	CONSTRAINT estoque_id_loja_fk FOREIGN KEY (id_loja)
+		REFERENCES loja (codigo),
+	CONSTRAINT estoque_id_item_fk FOREIGN KEY (id_item)
 		REFERENCES item (id)
+);
+
+CREATE TABLE melhoria (
+	loja SERIAL,
+	equipamento BIGINT,
+	
+	CONSTRAINT melhoria_pk PRIMARY KEY (loja, equipamento),
+	CONSTRAINT melhoria_loja_fk FOREIGN KEY (loja)
+		REFERENCES loja (codigo),
+	CONSTRAINT melhoria_equipamento_fk FOREIGN KEY (equipamento)
+		REFERENCES equip (id)
 );
 
 CREATE TABLE fala (
@@ -246,13 +314,6 @@ CREATE TABLE resiste (
 		REFERENCES resistencia (nivel)
 );
 
-CREATE TABLE equip (
-	id BIGINT,
-	tipo VARCHAR(10) NOT NULL CHECK (tipo IN ('arma', 'armadura')),
-	
-	CONSTRAINT equip_pk PRIMARY KEY (id)
-);
-
 CREATE TABLE equip_elem (
 	equipamento BIGINT,
 	elemento VARCHAR(20),
@@ -287,44 +348,16 @@ CREATE TABLE evento (
 		REFERENCES evento (codigo)
 );
 
-CREATE TABLE conecta (
-	pos_x1 SMALLINT,
-	pos_y1 SMALLINT,
-	area1 VARCHAR(50),
-	mapa1 VARCHAR(100),
-	pos_x2 SMALLINT,
-	pos_y2 SMALLINT,
-	area2 VARCHAR(50),
-	mapa2 VARCHAR(100),
-	
-	CONSTRAINT conecta_pk PRIMARY KEY (pos_x1, pos_y1, area1, mapa1, pos_x2, pos_y2, area2, mapa2),
-	CONSTRAINT quadrado1_fk FOREIGN KEY (pos_x1, pos_y1, area1, mapa1)
-		REFERENCES quadrado (pos_x, pos_y, area, mapa),
-	CONSTRAINT quadrado2_fk FOREIGN KEY (pos_x2, pos_y2, area2, mapa2)
-		REFERENCES quadrado (pos_x, pos_y, area, mapa)
-);
-
 CREATE TABLE drop (
 	item BIGINT,
 	evento BIGINT,
 	chance INTEGER,
 	
-	CONSTRAINT drop_pk PRIMARY KEY (item, evento),
+	CONSTRAINT drop_pk PRIMARY KEY (item, evento),	
 	CONSTRAINT drop_item_fk FOREIGN KEY (item)
 		REFERENCES item (id),
 	CONSTRAINT drop_evento_fk FOREIGN KEY (evento)
 		REFERENCES evento (codigo)
 );
 
-CREATE TABLE melhoria (
-	loja SERIAL,
-	equipamento BIGINT,
-	
-	CONSTRAINT melhoria_pk PRIMARY KEY (loja, equipamento),
-	CONSTRAINT melhoria_loja_fk FOREIGN KEY (loja)
-		REFERENCES loja (codigo),
-	CONSTRAINT melhoria_equipamento_fk FOREIGN KEY (equipamento)
-		REFERENCES equip (id)
-);
-
-
+COMMIT;
