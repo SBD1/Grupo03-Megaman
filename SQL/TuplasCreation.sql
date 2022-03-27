3ce807650135ce896edcb0384f77c8466b2bea43
andrelucasf <andre.lucas.lemos@hotmail.com>
2022-03-27 18:27:15 -0300

    Adicionando tuplas de personagens e mapas

---
+++ SQL/TuplasCreation.sql
@@ -0,0 +1,29 @@
+INSERT INTO personagem (nome, tipo) VALUES 
+('Mega Man', 'player' ),
+('Zero', 'player'),
+('Chill Penguin', 'npc'),
+('Thunder Slimer', 'npc'),
+('Armored Armadillo', 'npc'),
+('Utobolus', 'npc'),
+('Launcher Octopus', 'npc'),
+('RT-55J', 'npc'),
+('Sting Chameleon', 'npc'),
+('Storm Eagle', 'npc'),
+('Flame Mamooth', 'npc'),
+('Vile', 'npc'),
+('Bospider', 'npc'),
+('Rangda Bangda', 'npc'),
+('D-Rex', 'npc'),
+('Velguader', 'npc');
+
+INSERT INTO mapa (nome, descricao) VALUES 
+('AUTO ESTRADA', 'Vá seguindo em frente, só tenha cuidado com as plataformas que desabam'),
+('MONTANHA NEVADA', 'O maior problema aqui é na parte em que o chão desliza, você pode escorregar direto para o abismo'),
+('USINA DE ENERGIA', 'O maior problema aqui é a escuridão em alguns pontos, preste atenção na posição dos inimigos para não cair em buracos'),
+('MINAS', 'Cuidado com os buracos, cuidado também com o rolo compressor'),
+('OCEANO', 'Cuidado com espinhos no chão, principalmente quando enfrentar o robô que te empurra com um jato de água'),
+('TORRE', 'Tenha cuidado na hora que for subir no elevador para não morrer nos espinhos'),
+('FLORESTA', 'O maior problema aqui é na parte em que arvorés caem'),
+('CÉUS', 'Logo no começo quando estiver subindo nas plataformas móveis, pode ter problemas com robos que te jogam no abismo'),
+('FÁBRICA', 'Tenha cuidado na hora que for passar pelas máquinas'),
+('FORTALEZA DE SIGMA', 'Cuidado com as armadilhas espalhadas pelo castelo');
