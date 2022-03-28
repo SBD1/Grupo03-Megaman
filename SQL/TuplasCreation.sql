a063b6910813209ccda9d9e5eeb5dafb4dc9e518
Daniel Primo <42456421+danieldagerom@users.noreply.github.com>
2022-03-28 19:59:57 -0300

    Adiciona incerts e tuplas

--- SQL/TuplasCreation.sql
+++ SQL/TuplasCreation.sql
@@ -1,29 +1,30 @@
-INSERT INTO item (id, tipo) VALUES 
-('', '');
+INSERT INTO item (tipo) VALUES 
+('chave');
 
-INSERT INTO consumivel (id, nome, descricao, valor_compra, valor_venda, modificador_dano, modificador_energia) VALUES 
-('1', 'Arma-EXP-100', 'Material usado para melhorar as armas', '150', '100', '100', '0'),
-('2', 'Arma-EXP-250', 'Material usado para melhorar as armas', '250', '200', '250', '0'),
-('3', 'Arma-EXP-500', 'Material usado para melhorar as armas', '400', '300', '500', '0'),
-('4', 'Arma-EXP-1000', 'Material usado para melhorar as armas', '600', '500', '1000', '0');
+INSERT INTO consumivel (nome, descricao, valor_compra, valor_venda, modificador_dano, modificador_energia) VALUES 
+('Arma-EXP-100', 'Material usado para melhorar as armas', '150', '100', '100', '0'),
+('Arma-EXP-250', 'Material usado para melhorar as armas', '250', '200', '250', '0'),
+('Arma-EXP-500', 'Material usado para melhorar as armas', '400', '300', '500', '0'),
+('Arma-EXP-1000', 'Material usado para melhorar as armas', '600', '500', '1000', '0');
 
 INSERT INTO chave (id, nome, descricao, valor_compra, valor_venda) VALUES 
-('', '', '', '', '');
+('', 'Chave da Nave', 'Chave feita para destrancar a porta da nave', '100', '80');
 
 INSERT INTO equip (id, tipo) VALUES 
-('', '');
+('1','arma'),
+('2','armadura');
 
-INSERT INTO armadura (id, nome, tipo, descricao, valor_compra, valor_venda, valor_upgrade, nivel, energia, ataque, defesa) VALUES 
-('', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO armadura (nome, tipo, descricao, valor_compra, valor_venda, valor_upgrade, nivel, energia, ataque, defesa) VALUES 
+('Armadura contra explosões', 'Armadura feita para ser resistente contra explosões de inimigos', '300', '260', '200', '1', '50', '0', '100');
 
-INSERT INTO arma (id, nome, tipo, descricao, valor_compra, valor_venda, valor_upgrade, nivel, energia, ataque, defesa) VALUES 
-('', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO arma (nome, tipo, descricao, valor_compra, valor_venda, valor_upgrade, nivel, energia, ataque, defesa) VALUES 
+('Canhão Fumegante', 'Canhão feito de energia e metal. Arma lenta mas muito potente. Ideal para inimigos agrupados', '300', '260', '200', '1', '50', '400', '0');
 
-INSERT INTO inventario (id, tamanho) VALUES 
-('', '');
+INSERT INTO inventario (tamanho) VALUES 
+('50');
 
 INSERT INTO slot (id_inventario, pos, item) VALUES 
-('', '', '');
+('1', '1', '15');
 
 INSERT INTO personagem (nome, tipo) VALUES 
 ('Mega Man', 'player' ),
@@ -44,10 +45,10 @@ INSERT INTO personagem (nome, tipo) VALUES
 ('Velguader', 'npc');
 
 INSERT INTO player (nome, inventario, hp, energia, ataque, defesa, evasao, agilidade, creditos, arma, armadura) VALUES 
-('', '', '', '', '', '', '', '', '', '', '');
+('Mega Man', '50', '1000', '1000', '301', '301', '100', '100', '1000', '15', '2');
 
 INSERT INTO npc (nome, hp, energia, ataque, defesa, evasao, agilidade, arma, armadura) VALUES 
-('', '', '', '', '', '', '', '', '');
+('Utobolus', '300', '300', '100', '100', '30', '30', '1', '0');
 
 INSERT INTO mapa (nome, descricao) VALUES 
 ('AUTO ESTRADA', 'Vá seguindo em frente, só tenha cuidado com as plataformas que desabam'),
@@ -63,13 +64,16 @@ INSERT INTO mapa (nome, descricao) VALUES
 
 
 INSERT INTO area (mapa, nome, largura, altura) VALUES 
-('', '', '', '');
+('AUTO ESTRADA', 'Início', '120', '60');
 
 INSERT INTO quadrado (pos_x, pos_y, area, mapa, chance_batalha) VALUES 
-('', '', '', '', '');
+('10', '20', 'Início', 'AUTO ESTRADA', '50');
+
+INSERT INTO quadrado_tipo (pos_x, pos_y, area, mapa, tipo) VALUES 
+('10', '20', 'Início', 'AUTO ESTRADA', 'efeito');
 
 INSERT INTO quadrado_efeito (pos_x, pos_y, area, hp_mod, mp_mod) VALUES 
-('', '', '', '', '', '');
+('10', '20', 'Início', '10', '10', '10');
 
 INSERT INTO quadrado_item (pos_x, pos_y, area, mapa, item) VALUES 
 ('', '', '', '', '');
