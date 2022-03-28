cb4dd11d0a12612b012d3218222ae13784f5ad73
Wagner M Cunha <wagnermc506@gmail.com>
2022-03-27 22:16:20 -0300

    Iniciando implementação do jogo

---
+++ game/src/constants.py
@@ -0,0 +1,9 @@
+from pygame import Color
+
+class ColorConst:
+    BLACK = Color(0, 0, 0)
+    DEFAULT_COLOR = Color(100, 100, 100)
+    VISITED_COLOR = Color(0, 0, 255)
+    ITEM_COLOR = Color(0, 255, 0)
+    PLAYER_COLOR = Color(0, 0, 0)
+    NOWALK_COLOR = Color(255, 0, 0)
