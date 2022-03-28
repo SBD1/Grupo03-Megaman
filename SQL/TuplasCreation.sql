8cfe0c3efec505e13763d48201b208b9e658f8f8
andrelucasf <andre.lucas.lemos@hotmail.com>
2022-03-28 09:30:55 -0300

    Criacao de varias tuplas

--- SQL/TuplasCreation.sql
+++ SQL/TuplasCreation.sql
@@ -2,10 +2,10 @@ INSERT INTO item (id, tipo) VALUES
 ('', '');
 
 INSERT INTO consumivel (id, nome, descricao, valor_compra, valor_venda, modificador_dano, modificador_energia) VALUES 
-('1', 'Arma-EXP-100', 'Material usado para melhorar as armas', '150', '100', '500', '0'),
-('2', 'Arma-EXP-250', 'Material usado para melhorar as armas', '250', '200', '500', '0'),
+('1', 'Arma-EXP-100', 'Material usado para melhorar as armas', '150', '100', '100', '0'),
+('2', 'Arma-EXP-250', 'Material usado para melhorar as armas', '250', '200', '250', '0'),
 ('3', 'Arma-EXP-500', 'Material usado para melhorar as armas', '400', '300', '500', '0'),
-('4', 'Arma-EXP-1000', 'Material usado para melhorar as armas', '600', '500', '500', '0');
+('4', 'Arma-EXP-1000', 'Material usado para melhorar as armas', '600', '500', '1000', '0');
 
 INSERT INTO chave (id, nome, descricao, valor_compra, valor_venda) VALUES 
 ('', '', '', '', '');
@@ -43,6 +43,12 @@ INSERT INTO personagem (nome, tipo) VALUES
 ('D-Rex', 'npc'),
 ('Velguader', 'npc');
 
+INSERT INTO player (nome, inventario, hp, energia, ataque, defesa, evasao, agilidade, creditos, arma, armadura) VALUES 
+('', '', '', '', '', '', '', '', '', '', '');
+
+INSERT INTO npc (nome, hp, energia, ataque, defesa, evasao, agilidade, arma, armadura) VALUES 
+('', '', '', '', '', '', '', '', '');
+
 INSERT INTO mapa (nome, descricao) VALUES 
 ('AUTO ESTRADA', 'Vá seguindo em frente, só tenha cuidado com as plataformas que desabam'),
 ('MONTANHA NEVADA', 'O maior problema aqui é na parte em que o chão desliza, você pode escorregar direto para o abismo'),
@@ -54,3 +60,85 @@ INSERT INTO mapa (nome, descricao) VALUES
 ('CÉUS', 'Logo no começo quando estiver subindo nas plataformas móveis, pode ter problemas com robos que te jogam no abismo'),
 ('FÁBRICA', 'Tenha cuidado na hora que for passar pelas máquinas'),
 ('FORTALEZA DE SIGMA', 'Cuidado com as armadilhas espalhadas pelo castelo');
+
+
+INSERT INTO area (mapa, nome, largura, altura) VALUES 
+('', '', '', '');
+
+INSERT INTO quadrado (pos_x, pos_y, area, mapa, chance_batalha) VALUES 
+('', '', '', '', '');
+
+INSERT INTO quadrado_efeito (pos_x, pos_y, area, hp_mod, mp_mod) VALUES 
+('', '', '', '', '', '');
+
+INSERT INTO quadrado_item (pos_x, pos_y, area, mapa, item) VALUES 
+('', '', '', '', '');
+
+INSERT INTO conecta (pos_x1, pos_y1, area1, mapa1, pos_x2, pos_y2, area2, mapa2) VALUES 
+('', '', '', '', '', '', '', '');
+
+INSERT INTO altera (chave, pos_x, pos_y, area, mapa) VALUES 
+('', '', '', '', '');
+
+INSERT INTO destranca (chave, pos_x, pos_y, area, mapa) VALUES 
+('', '', '', '', '');
+
+INSERT INTO usa (id_chave, id_player) VALUES 
+('', '');
+
+INSERT INTO habilidade (id, descricao) VALUES 
+('', '');
+
+INSERT INTO loja (id, nome) VALUES 
+('', '');
+
+INSERT INTO estoque (id_loja, id_item) VALUES 
+('', '');
+
+INSERT INTO melhoria (loja, equipamento) VALUES 
+('', '');
+
+INSERT INTO fala (id, texto) VALUES 
+('', '');
+
+INSERT INTO bestiario (mapa, npc) VALUES 
+('', '');
+
+INSERT INTO elemento (nome) VALUES 
+('');
+
+INSERT INTO resistencia (nivel, porcentagem) VALUES 
+('', '');
+
+INSERT INTO resiste (elemento_a, elemento_b, intensidade) VALUES 
+('', '', '');
+
+INSERT INTO equip_elem (equipamento, elemento) VALUES 
+('', '');
+
+INSERT INTO equip_skill (equipamento, habilidade) VALUES 
+('', '');
+
+INSERT INTO evento (id, condicao, tipo, ev_anterior, acionamento_direito, desbloqueado) VALUES 
+('', '', '', '', '', '');
+
+INSERT INTO evento_chain (evento_a, evento_b) VALUES 
+('', '');
+
+INSERT INTO quadrado_evento (pos_x, pos_y, area, mapa, evento) VALUES 
+('', '', '', '', '');
+
+INSERT INTO drop (item, evento, chance) VALUES 
+('', '', '');
+
+INSERT INTO sessao (id, criado_em, player) VALUES 
+('', '', '');
+
+INSERT INTO sessao_quadrado (sessao, pos_x, pos_y, area, mapa, visado, item_pego, obstaculo_ativo, efeito_ativo) VALUES 
+('', '', '', '', '', '', '', '', '');
+
+INSERT INTO estado_evento (sessao, evento, desbloqueado, ativo) VALUES 
+('', '', '', '');
+
+INSERT INTO mapa_completo (sessao, mapa, completo) VALUES 
+('', '', '');
