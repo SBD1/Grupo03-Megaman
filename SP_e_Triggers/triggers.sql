b4f71d4e22b988cd92f2c65a9ef43857617db43d
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-04-18 16:37:11 -0300

    Criando triggers relacionados a criação de players

--- SP_e_Triggers/triggers.sql
+++ SP_e_Triggers/triggers.sql
@@ -135,3 +135,91 @@ DROP TRIGGER IF EXISTS check_quad_ev_tg ON quadrado_evento;
 CREATE TRIGGER check_quad_ev_tg
 BEFORE INSERT ON quadrado_evento
 FOR EACH ROW EXECUTE PROCEDURE check_quadrado_evento();
+
+
+
+-- player
+
+-- Atualizar tabela personagem automaticamente quando modifica player
+CREATE OR REPLACE FUNCTION update_player() RETURNS TRIGGER AS $update_player$
+
+BEGIN
+	IF (TG_OP = 'INSERT') THEN
+		INSERT INTO personagem(nome, tipo) VALUES (NEW.nome, 'player');
+		RETURN NEW;
+	ELSIF (TG_OP = 'UPDATE') THEN
+		IF(NEW.nome != OLD.nome) THEN
+			RAISE EXCEPTION 'Proibido modificar o nome';
+		END IF;
+	ELSIF (TG_OP = 'DELETE') THEN
+		DELETE FROM personagem WHERE nome = OLD.nome;
+		RETURN OK;
+	END IF;
+	RETURN NEW;
+END;
+$update_player$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS update_player_tg ON player;
+
+CREATE TRIGGER update_player_tg
+	AFTER INSERT OR UPDATE OR DELETE ON player
+	FOR EACH ROW
+	EXECUTE PROCEDURE update_player();
+
+
+
+-- Atualiza inventário automaticamente de acordo com o player
+CREATE OR REPLACE FUNCTION update_inventario_player() RETURNS TRIGGER AS $update_inventario_player$
+DECLARE
+	id_invent inventario.id%TYPE;
+BEGIN
+	IF (TG_OP = 'INSERT') THEN
+		INSERT INTO inventario(tamanho) VALUES (30) RETURNING id INTO id_invent;
+		NEW.inventario := id_invent;
+		RETURN NEW;
+	ELSIF (TG_OP = 'UPDATE') THEN
+		IF(NEW.id != OLD.id) THEN
+			RAISE EXCEPTION 'Proibido modificar o id';
+		END IF;
+	ELSIF (TG_OP = 'DELETE') THEN
+		DELETE FROM inventario WHERE id = OLD.inventario;
+		RETURN OK;
+	END IF;
+	RETURN NEW;
+END;
+$update_inventario_player$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS update_inventario_player_tg ON player;
+
+CREATE TRIGGER update_inventario_player_tg
+	BEFORE INSERT OR UPDATE OR DELETE ON player
+	FOR EACH ROW
+	EXECUTE PROCEDURE update_inventario_player();
+
+
+
+-- atualiza slots de acordo com mudanças nos inventários
+CREATE OR REPLACE FUNCTION update_slots_inventario() RETURNS TRIGGER AS $update_slots_inventario$
+BEGIN
+	IF (TG_OP = 'INSERT') THEN
+		INSERT INTO slot(id_inventario, pos)	
+		SELECT NEW.id, generate_series(1, NEW.tamanho);
+		RETURN NEW;
+	ELSIF (TG_OP = 'UPDATE') THEN
+		IF(NEW.id != OLD.id) THEN
+			RAISE EXCEPTION 'Proibido modificar o id';
+		END IF;
+	ELSIF (TG_OP = 'DELETE') THEN
+		DELETE FROM slot WHERE id_inventario = OLD.id;
+		RETURN OK;
+		END IF;
+	RETURN NEW;
+END;
+$update_slots_inventario$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS update_slots_inventario_tg ON player;
+
+CREATE TRIGGER update_slots_inventario_tg
+	AFTER INSERT OR UPDATE OR DELETE ON inventario
+	FOR EACH ROW
+	EXECUTE PROCEDURE update_slots_inventario();
